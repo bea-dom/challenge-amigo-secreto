c6e8de227e3229f100274e10742b2db4a79ca041
Beatriz Domingos de Oliveira <146678641+bea-dom@users.noreply.github.com>
2025-03-06 23:05:11 -0300

    Codigo final testado localmente

--- app.js
+++ app.js
@@ -1,1 +1,53 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos=[];
+
+function adicionarAmigo() {
+    let nomeAmigo = document.querySelector('input').value;
+    
+    if (nomeAmigo === ''){
+        console.log(`o usuario tentou inserir um valor vazio`);
+        alert(`Por favor, insira um nome.`);
+        
+    } else {
+        console.log(`o usario inseriu o nome ${nomeAmigo} na lista de sorteio`); 
+        amigos.push(nomeAmigo);
+        limparCampo();
+        atualizaListaTela();
+
+    }
+}
+
+function limparCampo(){
+    campo = document.querySelector('input');
+    campo.value = '';
+}
+
+function atualizaListaTela(){
+    let lista = document.getElementById('listaAmigos');
+    lista.innerHTML = ' ';
+
+    amigos.forEach(amigo => {
+        let li = document.createElement('li');
+        li.textContent = amigo;
+        lista.appendChild(li);
+    });
+
+}
+
+function sortearAmigo() {
+    tamanhoLista = amigos.length;
+
+    if (tamanhoLista == 0){
+        console.log(`o usario tentou realizar um sorteio sem inserir nomes a lista`);
+        alert(`Por favor, insira pelo menos um nome antes de realizar o sorteio`);
+    } else {
+        sorteado = parseInt(Math.random()*(tamanhoLista));
+        console.log(`o nome sorteado foi ${amigos[sorteado]} `); 
+        console.log(`o indice sorteado foi ${sorteado} `); 
+
+        let resultado = document.getElementById('resultado');
+        resultado.innerHTML = `O amigo secreto foi: ${amigos[sorteado]} `;
+
+        return 0;
+    }
+    
+}
